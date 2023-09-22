8ebd783fcbca26c90491c143468e62a9d749d8cb
Aniket-Pilankar <aniketpilankar1998@gmail.com>
2023-09-22 10:59:05 +0530

    add socket.io to application

--- server.js
+++ server.js
@@ -1,35 +1,73 @@
-require('dotenv').config()
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 
-const cors = require('cors')
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
-const connect = require('./server/configs/db');
+const connect = require("./server/configs/db");
 
-const { registerUser, loginUser } = require('./server/controllers/auth.controller');
-const userController = require('./server/controllers/user.controller')
+const {
+  registerUser,
+  loginUser,
+} = require("./server/controllers/auth.controller");
+const userController = require("./server/controllers/user.controller");
 const chatRoutes = require("./server/routes/chatRoutes");
 const messageRoutes = require("./server/routes/messageRoutes");
 
 app.post("/user/signUp", registerUser);
 app.post("/user/loginIn", loginUser);
 
-app.use('/api/user', userController);
+app.use("/api/user", userController);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
 
 const PORT = process.env.PORT || 4003;
 
-app.listen(PORT, async () => {
-    try {
-        const mongoConnection = await connect();
-        console.log(`Listening to port ${PORT}`);
-        console.log(`MongoDb is connected to ${mongoConnection.connection.host}`)
-    } catch (error) {
-        console.log('error:', error)
+const server = app.listen(PORT, async () => {
+  try {
+    const mongoConnection = await connect();
+    console.log(`Listening to port ${PORT}`);
+    console.log(`MongoDb is connected to ${mongoConnection.connection.host}`);
+  } catch (error) {
+    console.log("error:", error);
+  }
+});
+const io = require("socket.io")(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
 
-    }
-})
+io.on("connection", (socket) => {
+  console.log("Connected to socket.io");
+  socket.on("setup", (userData) => {
+    socket.join(userData._id);
+    socket.emit("connected");
+  });
+
+  socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log("User Joined Room: " + room);
+  });
+  
+  socket.on("new message", (newMessageRecieved) => {
+    var chat = newMessageRecieved.chat;
+
+    if (!chat.users) return console.log("chat.users not defined");
+
+    chat.users.forEach((user) => {
+      if (user._id == newMessageRecieved.sender._id) return;
+
+      socket.in(user._id).emit("message recieved", newMessageRecieved);
+    });
+  });
+
+  socket.off("setup", () => {
+    console.log("USER DISCONNECTED");
+    socket.leave(userData._id);
+  });
+});
